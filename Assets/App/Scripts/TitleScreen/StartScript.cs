b6920e02edc9881a0e8c0d3baf6ff70871a23dfc
Edward <edward6882990@gmail.com>
2015-09-07 15:21:18 -0700

    Rebased on master

---
+++ Assets/App/Scripts/TitleScreen/StartScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class StartScript : MonoBehaviour
+{
+	private GUISkin skin;
+	
+	void Start()
+	{
+		skin = Resources.Load ("StartGUISkin") as GUISkin;
+	}
+	
+	void OnGUI()
+	{
+		const int buttonWidth = 84;
+		const int buttonHeight = 60;
+		
+		GUI.skin = skin;
+		
+		// Determine the button's place on screen
+		// Center in X, 2/3 of the height in Y
+		Rect buttonRect = new Rect(
+			Screen.width / 2 - (buttonWidth / 2),
+			(2 * Screen.height / 4) - (buttonHeight / 2),
+			buttonWidth,
+			buttonHeight
+			);
+		
+		// Draw a button to start the game
+		if(GUI.Button(buttonRect,""))
+		{
+			LoadGameScene();
+		}
+	}
+
+	public void LoadGameScene(){
+		Application.LoadLevel("Game");
+	}
+}
