6d511de6842f64362057f7d515ee081b590be9c1
Edward <edward6882990@gmail.com>
2015-09-11 05:18:29 -0700

    renamed StartScript into StartButton.cs

---
+++ Assets/App/Scripts/TitleScreen/StartButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartButton : MonoBehaviour {
+	public void LoadGameScene(){
+		Application.LoadLevel("Game");
+	}
+}
